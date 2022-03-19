8d4ccc068c657bfb23f73bdc34405c890f439197
nanometre <benedict.wong@trentglobal.edu.sg>
2022-03-19 15:15:07 +0000

    Added read functionality

--- src/index.js
+++ src/index.js
@@ -1,12 +1,48 @@
-const express = require('express')
-const { append, render } = require('express/lib/response')
+const express = require('express');
+const MongoUtil = require('./MongoUtil.js');
+const ObjectId = require('mongodb').ObjectId;
+const cors = require('cors');
+require('dotenv').config();
+let app = express();
 
-let app = express()
+app.use(express.json());
+app.use(cors());
 
-app.get("/", function(req, res){
-    res.send("Hello World")
-})
+const mongoUri = process.env.MONGO_URI;
+const COLLECTION_NAME = 'animals';
 
-app.listen(3000, () =>{
-    console.log('Server has started')
-})
+async function main() { 
+    await MongoUtil.connect(mongoUri, 'pet_adoption')
+
+    // test page for default link
+    app.get("/", function (req, res) {
+        res.send("Express app is functional")
+    })
+
+    // GET: Return all animals in the DB
+    app.get("/animals", async function (req, res){
+        let db = MongoUtil.getDB();
+        let animalRecords = await db.collection(COLLECTION_NAME)
+                                    .find()
+                                    .toArray();
+        res.send(animalRecords)
+    })
+
+    // POST: Add new animals in the DB
+    app.post("/animals", async function (req, res) {
+        let {name, gender} = req.body
+
+        let db = MongoUtil.getDB();
+        await db.collection(COLLECTION_NAME).insertOne({
+            name,
+            gender
+        })
+        res.send("Response received")
+    })
+
+    app.listen(3000, () => {
+        console.log('Server has started')
+    })
+}
+
+main();
